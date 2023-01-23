2d9d913ef28f7d7d357c089bfdbe969f0219ce95
ashw-ani <ashwanisharma72002@gmail.com>
2023-01-23 17:07:44 +0530

    changed db string

--- src/main/java/com/mapApp/mapApp/controller/LocationController.java
+++ src/main/java/com/mapApp/mapApp/controller/LocationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+//@CrossOrigin
 @RequestMapping("/registerStations")
 public class LocationController {
     @Autowired
